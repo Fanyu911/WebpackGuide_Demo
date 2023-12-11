ae178f08915c400786d2ae2c13e4b16cf182be1c
Fanyu <yefanyu911@gmail.com>
2023-12-11 17:52:43 +0800

    4.开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,27 @@ const path = require('path');
 const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dict',
+  },
   plugins: [
     new htmlPlugin({
-      title: '管理输出',
+      title: 'Development',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
