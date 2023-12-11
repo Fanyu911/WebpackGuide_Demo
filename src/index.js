5d2e23eb689a5fcfb833cafadfa25def4321e091
Fanyu <yefanyu911@gmail.com>
2023-12-11 17:13:22 +0800

    3.管理输出

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
-
+import printFun from './print.js';
 function component() {
   const element = document.createElement('div');
 
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const btn = document.createElement('button');
+  btn.innerHTML = 'print';
+  btn.onclick = printFun;
+
+  element.appendChild(btn);
 
   return element;
 }
