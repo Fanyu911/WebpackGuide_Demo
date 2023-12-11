44bc17e180a8934e240af3ffca39922ddcc0a3c9
Fanyu <yefanyu911@gmail.com>
2023-12-11 16:04:11 +0800

    2.资源管理

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import icon from './my-img.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
 
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  const img = new Image();
+  img.src = icon;
+
+  element.appendChild(img);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
 const path = require('path');
 
-// module.exports = {
-//     entry: './src/index.js',
-//     output: {
-//       filename: 'main.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-//   };
-
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
   },
 };
