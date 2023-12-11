6c45e2ca7399d95c806cbdbfe6ec43f3c289b629
Fanyu <yefanyu911@gmail.com>
2023-12-11 16:14:59 +0800

    2.回退处理

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import _ from 'lodash';
-import './style.css';
-import icon from './my-img.png';
-import Data from './data.xml';
-import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -10,14 +6,6 @@ function component() {
   // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  const img = new Image();
-  img.src = icon;
-
-  element.appendChild(img);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
