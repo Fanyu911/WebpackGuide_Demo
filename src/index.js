ffecb7cadbe118392fc8249f9b7db09cbc9ada55
Fanyu <yefanyu911@gmail.com>
2023-12-15 16:31:21 +0800

    5.代码分离

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-import _ from 'lodash';
-import printFun from './print.js';
-function component() {
-  const element = document.createElement('div');
-
-  // 执行这一行需要引入 lodash（目前通过 script 脚本引入）
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const btn = document.createElement('button');
-  btn.innerHTML = 'print';
-  btn.onclick = printFun;
-
-  element.appendChild(btn);
-
-  return element;
+function getComponent() {
+  return import('lodash')
+    .then(({ default: _ }) => {
+      const element = document.createElement('div');
+      element.innerHTML = _.join(['Hello', 'webpack']);
+      return element;
+    })
+    .catch((error) => 'An erroe');
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,13 @@
 const path = require('path');
-const htmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dict',
-  },
-  plugins: [
-    new htmlPlugin({
-      title: 'Development',
-    }),
-  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
