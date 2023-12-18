55803e7a360d1b525ec546037e6293b694eb1290
Fanyu <yefanyu911@gmail.com>
2023-12-18 09:35:04 +0800

    6.缓存

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-function getComponent() {
-  return import('lodash')
-    .then(({ default: _ }) => {
-      const element = document.createElement('div');
-      element.innerHTML = _.join(['Hello', 'webpack']);
-      return element;
-    })
-    .catch((error) => 'An erroe');
+import _ from 'lodash';
+import Print from './print';
+
+function component() {
+  const element = document.createElement('div');
+
+  // lodash 现在使用 import 引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.onclick = Print.bind(null, 'Hello webpack!');
+
+  return element;
 }
 
-getComponent().then((component) => {
-  document.body.appendChild(component);
-});
+document.body.appendChild(component());
